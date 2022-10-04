e065fd6442f75366df40ce8399ae2bf1fb3d32f4
Christian Danizek <christian@danizek.com>
2022-10-04 15:17:00 +0200

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using NSec.Cryptography;
+
+namespace BP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NSec.Cryptography.Key? clientKeyPair;
+        Server? server;
+        
+        public MainWindow()
+        {
+            InitializeComponent();            
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            statusText.Content = "Generating keypair...";
+            clientKeyPair = NSec.Cryptography.Key.Create(SignatureAlgorithm.Ed25519);
+            
+            statusText.Content = "Starting server...";
+            RunServer();
+            statusText.Content = "Idle";
+        }
+
+        private void RunServer()
+        {
+            server = new Server(this);
+            Thread serverThread = new Thread(server.Start);
+            serverThread.Start();
+        }
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace BP
+{
+    internal class Server
+    {
+        private int? port;
+        private TcpListener? tcpListener;
+        private MainWindow mainWindow;
+
+        public Server(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        public void Start()
+        {
+            tcpListener = new TcpListener(System.Net.IPAddress.Any, 0);
+            tcpListener.Start();
+            port = ((IPEndPoint)tcpListener.LocalEndpoint).Port;
+            Application.Current.Dispatcher.Invoke(new Action(() => { mainWindow.statusPortText.Content = port.ToString(); }));
+        }
+
+        public int? Port { get { return port; } }
+    }
+}

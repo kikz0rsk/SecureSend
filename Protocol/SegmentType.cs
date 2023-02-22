b72c4f6fb2cb9bd57d24d572202554830ded28bd
Christian Danizek <christian@danizek.com>
2023-02-22 15:26:51 +0100

    removed disconnect and stop segments

--- Protocol/SegmentType.cs
+++ Protocol/SegmentType.cs
@@ -9,9 +9,7 @@
         PREPARE_TRANSFER = 4,
         ACK = 5,
         NACK = 6,
-        DISCONNECT = 7,
-        STOP = 8,
-        PASSWORD_AUTH_REQ = 9,
-        PASSWORD_AUTH_RESP = 10,
+        PASSWORD_AUTH_REQ = 7,
+        PASSWORD_AUTH_RESP = 8,
     }
 }
